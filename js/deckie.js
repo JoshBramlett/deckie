4029be1d1a26a1c1a651b572c5062c2816c12cc6
Josh Bramlett <bramsquad@mac.com>
2014-04-01 16:44:15 -0500

    HearthStats Import among numerous other changes
    Completely reworked the background page and moved a lot of the logic
    outside the main script.  Added deck uids rather than indexing by name.
     Added a card secrets page in addition to adding support for user
    settings.

--- js/deckie.js
+++ js/deckie.js
@@ -115,109 +115,25 @@ var HearthStoneArtist = kendo.data.Model.define( {
 	}
 });
 
-/****************************************************
- *
- *             DECKIE DATASOURCE OBJECTS
- *
- ****************************************************/
-
-/* static data source for hearthstone classes */
-/* (e.g. Priest, Paladin, etc.) */
-var dsClasses = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneClass
-	}
-});
-
-/* static data source for hearthstone artists */
-var dsArtists = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneArtist
-	}
-});
-
-/* static data source for hearthstone card qualities */
-/* (e.g. Epic, Legendary, etc.) */
-var dsCardQualities = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCardQuality
-	}
-});
-
-/* static data source for hearthstone card types */
-/* (e.g. Spell, Minion, etc.) */
-var dsCardTypes = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCardType
-	}
-});
-
-/* static data source for hearthstone card races */
-/* (e.g. Beast, Murloc, etc.) */
-var dsCardRaces = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCardRace
-	}
-});
-
-/* static data source for hearthstone card sets */
-/* (e.g. Basic, Expert, etc.) */
-var dsCardSets = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCardSet
-	}
-});
-
-/* data source for the quick card search */
-var dsCardSearch = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCard
-	}
-});
-
-/* data source for the filterable "select from" cards */
-var dsCardFilterable = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCard
-	},
-	sort: [{ field: "cost", dir: "asc" },{ field: "name", dir: "asc" }]
-});
-
-/* data source for the user selected deck */
-var dsCardSelected = new kendo.data.DataSource({
-    schema: {
-		model: HearthStoneCard
-	},
-	sort: [{ field: "cost", dir: "asc" },{ field: "name", dir: "asc" }],
-	aggregate: { field: "count", aggregate: "sum" }
-});
-
-/* user decks stored on the client */
-var dsDecks = new kendo.data.DataSource({
-    schema: {
-		model: {
-			id: "name",
+var DeckBuilder = kendo.data.Model.define( {
+	id: "id",
+	fields: {
+		id:			{ type: "string" },
+		name: 		{ type: "string" },
+		classid: 	{ type: "number" },
+		cards: {
 			fields: {
-				name: {
-					type: "string"
-				},
-				classid: {
-					type: "number"	
-				},
-				cards: {
-					fields: {
-						id: {
-							type: "number"
-						},
-						count: {
-							type: "number"
-						}
-					},
-					nullable: false,
-					defaultValue: {}
-				}
-			}
-		}
+				id: 	{ type: "number" },
+				count: 	{ type: "number" }
+			},
+			nullable: false,
+			defaultValue: {}
+		},
+		hsuid:		{ type: "number" },
+		wins:		{ type: "number" },
+		losses:		{ type: "number" },
+		created:	{ type: "number" },
+		hsuid:		{ type: "number" }
 	}
 });
 
@@ -339,12 +255,21 @@ $.extend(kendo.data.DataSource.prototype, {
 		// no match - return null
 		return null;
 	},
-	addCard: function(dataItem) {
-		// make sure we don't have a full deck already
-		if (this.aggregates().count && this.aggregates().count.sum >= 30) {
+	addCard: function(dataItem, count_override) {
+		// override is only set when contructing a deck from an exising source.
+		// no need to do error checking
+		if (count_override != undefined) {
+			dataItem.count = count_override;
+			this.add(dataItem);
+			return;
+		} 
+		
+		// make sure we don't have a full deck
+		var total = this.aggregates().count;
+		if (total && total.sum >= 30) {
 			return false;
 		}
-	
+
 		var count = (dataItem.count == undefined) ? 1 : dataItem.count + 1;
 
 		// check if it's a valid addition
@@ -356,21 +281,20 @@ $.extend(kendo.data.DataSource.prototype, {
 		
 		// only add if record is new - update the count otherwise
 		if (count > 1) { 
-			this.get(dataItem.id).set("count", count);
+			dataItem.set("count", count);
 		} else {	
 			this.add(dataItem);
-			this.get(dataItem.id).set("count", count);
+			dataItem.set("count", count);
 		}
 		
 		return count;
 	},
 	cleanDeck: function() {
-		// sets underlying data (shared across other data sources)
 		var data = this.data();
-        for (var i = 0; i < data.length; i++) {
-            this.get(data[i].id).set("count", undefined);
+		for (var i = 0; i < data.length; i++) {
+            data[i].count = undefined;
         }
-        
+	
         // remove any existing
         this.data([]);
 	},
@@ -388,6 +312,34 @@ $.extend(kendo.data.DataSource.prototype, {
         return deck;
 	}
 });
+
+/****************************************************
+ *
+ *                 RANDOM EXTENSIONS
+ *
+ ****************************************************/
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+String.prototype.sluggify = function() {
+  	this = this.replace(/^\s+|\s+$/g, ''); // trim
+  	this = this.toLowerCase();
+  
+  	// remove accents, swap ñ for n, etc
+  	var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  	var to   = "aaaaeeeeiiiioooouuuunc------";
+  	for (var i=0, l=from.length ; i<l ; i++) {
+    	this = this.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+	}
+
+	this = this.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    	.replace(/\s+/g, '-') // collapse whitespace and replace by -
+		.replace(/-+/g, '-'); // collapse dashes
+
+	return this;
+};
 
 /****************************************************
  *
@@ -560,3 +512,337 @@ DeckFilter.prototype.getFilter = function() {
 	
 	return filter;
 }
+
+/****************************************************
+ *
+ *                	   THE APP
+ *
+ ****************************************************/
+
+function App() {
+	/* variable card ds used for deck building, etc. */
+	this._ds_cards = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCard },
+		sort: [{ field: "cost", dir: "asc" },{ field: "name", dir: "asc" }],
+		aggregate: { field: "count", aggregate: "sum" }
+	});
+	
+	/* immutable card list */
+	this._ds_cards_immutable = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCard },
+		sort: [{ field: "cost", dir: "asc" },{ field: "name", dir: "asc" }]
+	});
+	
+	/* static data source for hearthstone classes */
+	/* (e.g. Priest, Paladin, etc.) */
+	this._ds_classes = new kendo.data.DataSource({
+	    schema: { model: HearthStoneClass }
+	});
+	
+	/* static data source for hearthstone artists */
+	this._ds_artists = new kendo.data.DataSource({
+	    schema: { model: HearthStoneArtist }
+	});
+	
+	/* static data source for hearthstone card qualities */
+	/* (e.g. Epic, Legendary, etc.) */
+	this._ds_card_qualities = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCardQuality }
+	});
+	
+	/* static data source for hearthstone card types */
+	/* (e.g. Spell, Minion, etc.) */
+	this._ds_card_types = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCardType }
+	});
+	
+	/* static data source for hearthstone card races */
+	/* (e.g. Beast, Murloc, etc.) */
+	this._ds_card_races = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCardRace }
+	});
+	
+	/* static data source for hearthstone card sets */
+	/* (e.g. Basic, Expert, etc.) */
+	this._ds_card_sets = new kendo.data.DataSource({
+	    schema: { model: HearthStoneCardSet }
+	});
+	
+	/* user created decks */
+	this._ds_decks = new kendo.data.DataSource({
+	    schema: { model: DeckBuilder }
+	});
+}
+
+App.prototype.init = function(origin, source, snapshot) {
+	this._origin = origin;
+	this._source = source;
+
+	this._us_default_deck = snapshot.settings.default_deck;
+	this._us_default_page = snapshot.settings.default_page;
+	this._us_hs_userkey = snapshot.settings.hs_userkey;
+	
+	this._ds_cards_immutable.data(snapshot.cards.list);
+	this._ds_classes.data(snapshot.classes);
+	this._ds_artists.data(snapshot.artists);
+	this._ds_card_qualities.data(snapshot.cards.qualities);
+	this._ds_card_types.data(snapshot.cards.types);
+	this._ds_card_races.data(snapshot.cards.races);
+	this._ds_card_sets.data(snapshot.cards.sets);
+	this._ds_decks.data(snapshot.decks);
+}
+
+App.prototype.getDefaultDeck = function() { 
+	return this._us_default_deck;
+}
+
+App.prototype.setDefaultDeck = function(deckid) {
+	this._us_default_deck = deckid;
+}
+
+App.prototype.getDefaultPage = function() {
+	return this._us_default_page;
+}
+
+App.prototype.setDefaultPage = function(pageid) {
+	this._us_default_page = pageid;
+}
+
+App.prototype.getHearthStatsUserKey = function() {
+	return this._us_hs_userkey;
+}
+
+App.prototype.setHearthStatsUserKey = function(userkey) {
+	this._us_hs_userkey = userkey;
+}
+
+App.prototype.getCardDataSource = function() {
+	return this._ds_cards;
+}
+
+App.prototype.getCardImmutableDataSource = function() {
+	return this._ds_cards_immutable;
+}
+
+App.prototype.getDeckDataSource = function() {
+	return this._ds_decks;
+}
+
+App.prototype.getClassDataSource = function() {
+	return this._ds_classes;
+}
+
+App.prototype.getArtistDataSource = function() {
+	return this._ds_artists;
+}
+
+App.prototype.getCardQualityDataSource = function() {
+	return this._ds_card_qualities;
+}
+
+App.prototype.getCardTypeDataSource = function() {
+	return this._ds_card_types;
+}
+
+App.prototype.getCardRaceDataSource = function() {
+	return this._ds_card_races;
+}
+
+App.prototype.getCardSetDataSource = function() {
+	return this._ds_card_sets;
+}
+
+App.prototype.getImmutableFilter = function() {
+	return this._card_filter;
+}
+
+App.prototype.setImmutableFilter = function(filter) {
+	this._card_filter = filter;
+	this._ds_cards_immutable.filter(this._card_filter.getFilter());
+}
+
+App.prototype.refreshImmutableFilter = function() {
+	this._ds_cards_immutable.filter(this._card_filter.getFilter());
+}
+
+// reset the card datasources to their original state
+App.prototype.cardsPristine = function() {
+	this._ds_cards.cleanDeck();
+	if (this._ds_cards_immutable.filter() != undefined) {
+    	this._ds_cards_immutable.filter({});
+	}
+	this._card_filter = undefined;
+}
+
+App.prototype.selectCardsFromDeck = function(deck) {
+	this.cardsPristine();
+	for (var i = 0; i < deck.cards.length; ++i) {
+		var card = this._ds_cards_immutable.get(deck.cards[i].id);
+		this._ds_cards.addCard(card, deck.cards[i].count);
+	}
+}
+
+// for sending messages to the proxy 
+App.prototype.toProxy = function(message, data) {
+	var data = data || {};
+	this._source.postMessage({
+		message: message,
+		data: data
+	}, this._origin);
+}
+
+// hides current panel and displays new panel
+App.prototype.showPanel = function(dom, width, callback) {
+	var me = this;
+	if (!this._active_panel) {
+		// no panel selected
+		$(dom).slideToggleH();
+		this._active_panel = dom;
+		
+		if (callback) {
+			callback(true);
+		}
+	} else if (dom.get(0) != this._active_panel.get(0)) {
+		// hide current panel before opening new
+		$(this._active_panel).slideToggleH(function() {
+			me.toProxy("iframe-width", width);
+			$(dom).slideToggleH(function() {
+				me._active_panel = dom;
+			
+				if (callback) {
+					callback(true);
+				}
+			});
+			
+		});
+	} else {
+		// same panel was selected - do nothing
+		if (callback) {
+			callback(true);
+		}
+	}
+}
+
+App.prototype.decodeHearthPwn = function(url) { 
+	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	if (!regexp.test(url)) {
+		return false;
+	}
+	
+	var endURI = url.lastIndexOf("/");
+	var hash = url.indexOf("#");
+	if (endURI < 0 || hash < 0) {
+		return false;
+	}
+	
+	var classList = ["druid","hunter","mage","paladin","priest","rogue","shaman","warlock","warrior"];
+	var className = url.substring(endURI+1, hash);
+	var classid = classList.indexOf(className) + 1;
+	if (classid < 0) {
+		return false;
+	}
+	
+	if (url.endsWith(";")) {
+		url = url.substring(0, url.length-1);
+	}
+	
+	var encoded = url.substring(hash+1);
+	var cards = encoded.split(";");
+	if (cards.length < 1) {
+		return false;
+	}
+	
+	this._ds_cards.cleanDeck();
+	for (var i = 0; i < cards.length; ++i) {
+		var card = cards[i].split(":");
+		if (card.length != 2) {
+			this._ds_cards.cleanDeck(); 
+			return false;
+		}
+		
+		var cardid = parseInt(card[0],10);
+		var cardcnt = parseInt(card[1],10);
+		if (cardid == NaN || cardcnt == NaN || cardcnt < 1 || cardcnt > 2) {
+			this._ds_cards.cleanDeck(); 
+			return false;
+		}
+		
+		var cardObj = this._ds_cards_immutable.data().filter(function (f) {
+			return f.hpid == cardid;
+		});
+		
+		if (cardObj.length != 1) {
+			this._ds_cards.cleanDeck(); 
+			return false;
+		}
+		
+		this._ds_cards.addCard(cardObj[0], cardcnt);
+	}
+
+	return classid;
+}
+
+App.prototype.decodeHearthStats = function(response) { 
+
+	var to_import = [];
+
+	var decks = response;
+	for (var i = 0; i < decks.length; ++i) {
+		this._ds_cards.cleanDeck();
+		var cards = decks[i].cardstring.split(",");
+		if (cards.length >= 1) {
+			for (var j = 0; j < cards.length; ++j) {
+				var card = cards[j].split("_");
+				var cardid = parseInt(card[0],10);
+				var cardcnt = parseInt(card[1],10);
+				
+				var cardObj = this._ds_cards_immutable.data().filter(function (f) {
+					return f.hsid == cardid;
+				});
+
+				this._ds_cards.addCard(cardObj[0], cardcnt);
+			}
+		}
+		
+		var import_entry = {
+			hsuid: decks[i].unique_deck_id,
+			name: decks[i].name,
+			classid: decks[i].klass_id,
+			cards: _app.getCardDataSource().serialize(),
+			wins: decks[i].user_num_wins,
+			losses: decks[i].user_num_losses,
+			created: decks[i].created_at,
+			lastsync: Date.now(),
+		};
+		
+		to_import.push(import_entry);
+	}
+
+	return to_import;
+}
+
+/*
+{
+    "active": null,
+    "cardstring": "172_2,58_1,92_2,84_1,4_1,30_2,105_1,374_2,294_2,308_2,40_2,258_1,346_1,73_2,127_2,368_1,216_1,289_2,142_1,241_1",
+    "created_at": "2014-02-18T22:48:42Z",
+    "decklink": "",
+    "id": 15645,
+    "is_public": false,
+    "klass_id": 7,
+    "loses": 0,
+    "name": "Control 2/18/14",
+    "notes": "<p>\r\n\t http://www.hearthhead.com/deck=28174&amp;edit\r\n</p>",
+    "race": "Shaman",
+    "slot": null,
+    "slug": "control-2-18-14",
+    "unique_deck_id": 7219,
+    "updated_at": "2014-03-21T02:40:09Z",
+    "user_id": 14714,
+    "user_num_losses": 21,
+    "user_num_matches": 41,
+    "user_num_wins": 20,
+    "user_winrate": 48.7805,
+    "wins": 20
+},
+*/
